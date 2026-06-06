f97ab522fb969933f40c8e41cb8a5a265424f363
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-06-06 17:35:55 +0900

    Update cxr to v0.1.4

--- Formula/cxr.rb
+++ Formula/cxr.rb
@@ -2,7 +2,7 @@ class Cxr < Formula
   desc "A tool to generate a directory structure from a YAML template"
   homepage "https://github.com/suzuuuuu09/cxr"
   url "https://github.com/suzuuuuu09/cxr/releases/download/v0.1.4/cxr-0.1.4.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  sha256 "25fb440c8b699703d724db17bcd59e69ae605133a3749356dc4b80b6494f6ce9"
   license "MIT"
 
   depends_on "rust" => :build
